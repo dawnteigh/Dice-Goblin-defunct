456a1923a35d779e96066359aff3954fef1ed7a7
Donte Ladatto <donte279@gmail.com>
2022-05-26 09:32:06 -0500

    get array of values for each die

--- src/DiePage.js
+++ src/DiePage.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const DiePage = ({ die }) => {
+  const numOfButtons = parseInt(die.replace(/\D/g, ''))
+  const looper = (num) => {
+    let numArray = [];
+    for (let i = 1; i <= num; i++) {
+      numArray.push(i)
+    }
+    return numArray
+  }
+  const diceValues = looper(numOfButtons)
+
+  useEffect(() => {
+    console.log(diceValues)
+  }, [])
+
   return (
-    <div>{die}</div>
+    <div>
+      {die}
+    </div>
   )
 }
 
